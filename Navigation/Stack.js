484c3be00499ba7f3098df3f2ae32350555fe76b
Hadeer Ali <hadeer.ali518@gmail.com>
2020-04-05 01:42:12 +0200

    test

--- Navigation/Stack.js
+++ Navigation/Stack.js
@@ -1,18 +1,21 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import Init from '../Screens/Init'
+import { createStackNavigator } from 'react-navigation-stack';
+import Init from '../Screens/Init';
+import Signup from '../Screens/Signup'
 
 
 const stack = createStackNavigator(
-    {
-      Init : Init , 
-    },
-    {
-      initialRouteName: 'Init',
-      headerMode: 'none',
-    },
-    
-  )
+  {
+    Init: Init,
+    Signup: Signup,
 
-  const Stack=createAppContainer(stack);
- export default Stack
+  },
+  {
+    initialRouteName: 'Signup',
+    headerMode: 'none',
+  },
+
+)
+
+const Stack = createAppContainer(stack);
+export default Stack
