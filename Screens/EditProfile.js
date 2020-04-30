fcba9fefcbbbf238b9afc4599c43d54cc0353a22
LamiaaAssy <lamiaa.assy@gmail.com>
2020-04-30 04:23:05 +0200

    colorerror

--- Screens/EditProfile.js
+++ Screens/EditProfile.js
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
     },
     name: {
         fontSize: 16,
-        color: Colors.textCard,
+        color: '#7C7C7C',
         fontFamily: 'Montserrat-Medium',
         marginTop: 7,
         // backgroundColor: "red"
