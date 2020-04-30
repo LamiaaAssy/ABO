0dc54a9bd450e5620efb6fce548646e8ec737db4
samarsayed998@gmail.com <samarsayed998@gmail.com>
2020-04-30 02:24:54 +0200

    update samar tasks

--- Navigation/Stack.js
+++ Navigation/Stack.js
@@ -1,7 +1,7 @@
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Init from '../Screens/Init';
-//import Signup from '../Screens/Signup'
+import Signup from '../Screens/Signup'
 import Calendar from '../Screens/Calendar'
 import AllRequests from '../Screens/AllRequests'
 import ChatHome from '../Screens/Chat/ChatHome'
@@ -18,7 +18,7 @@ const stack = createStackNavigator(
   {
     Init: Init,
     Calendar: Calendar,
-    //Signup: Signup,
+    Signup: Signup,
     AllRequests: AllRequests,
     ChatHome: ChatHome,
     ChatView: ChatView,
