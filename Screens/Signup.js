05d4a9e0f4bda71f852511f5ccbb9ec5cb9372f3
aisha-samir <60828233+aisha-samir@users.noreply.github.com>
2020-05-09 23:40:23 -0800

    firebase-integration

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -236,24 +236,24 @@ export default class Signup extends Component {
         }
     }
 
-    test = async () => {
-        auth()
-            .createUserWithEmailAndPassword('sarah.lane@gmail.com', 'SuperSecretPassword!')
-            .then(() => {
-                console.log('User account created & signed in!');
-            })
-            .catch(error => {
-                if (error.code === 'auth/email-already-in-use') {
-                    console.log('That email address is already in use!');
-                }
+    // test = async () => {
+    //     auth()
+    //         .createUserWithEmailAndPassword('sarah.lane@gmail.com', 'SuperSecretPassword!')
+    //         .then(() => {
+    //             console.log('User account created & signed in!');
+    //         })
+    //         .catch(error => {
+    //             if (error.code === 'auth/email-already-in-use') {
+    //                 console.log('That email address is already in use!');
+    //             }
 
-                if (error.code === 'auth/invalid-email') {
-                    console.log('That email address is invalid!');
-                }
+    //             if (error.code === 'auth/invalid-email') {
+    //                 console.log('That email address is invalid!');
+    //             }
 
-                console.error(error);
-            });
-    }
+    //             console.error(error);
+    //         });
+    // }
 
 
     render() {
